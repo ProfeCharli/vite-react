9c7c1cecdf248229a36c5a4f1fe06c84a8a1d904
Profe Charli <carlossanchez@uas.edu.mx>
2025-11-12 13:23:24 -0700

    Second Try

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          Contador está en: {count}
+          Contador está en de nuevo: {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
